b7c0ac2da4393f53c6005a5ba0b93e6382be8571
tracer-oscpu2021 <tracer@oscpu.org>
2021-11-30 11:01:20 +0800

    jiangshijie 20210000    Static hostname: iscaearth          Icon name: computer-server            Chassis: server         Machine ID: 5b79f3682a3c4191a08aa2b25d1a612e            Boot ID: e382fb8501284c3492133ca9bd511cb5   Operating System: Ubuntu 20.04.3 LTS             Kernel: Linux 5.8.0-53-generic       Architecture: x86-64  11:01:20 up 185 days, 12:32, 26 users,  load average: 13.85, 18.14, 16.53

--- projects/chisel_cpu_diff/vsrc/SimTop.v
+++ projects/chisel_cpu_diff/vsrc/SimTop.v
@@ -942,12 +942,11 @@ module Core(
   reg [31:0] _RAND_0;
   reg [31:0] _RAND_1;
   reg [31:0] _RAND_2;
-  reg [63:0] _RAND_3;
-  reg [31:0] _RAND_4;
-  reg [63:0] _RAND_5;
-  reg [31:0] _RAND_6;
+  reg [31:0] _RAND_3;
+  reg [63:0] _RAND_4;
+  reg [31:0] _RAND_5;
+  reg [63:0] _RAND_6;
   reg [63:0] _RAND_7;
-  reg [63:0] _RAND_8;
 `endif // RANDOMIZE_REG_INIT
   wire [31:0] nxt_pc_io_pc; // @[Core.scala 20:22]
   wire [2:0] nxt_pc_io_imm_type; // @[Core.scala 20:22]
@@ -1079,7 +1078,6 @@ module Core(
   wire [63:0] _GEN_21 = _regfile_io_rd_en_T & _io_dmem_en_T ? _GEN_13 : {{32'd0}, _regfile_io_rd_data_T_16}; // @[Core.scala 133:74]
   wire [63:0] _GEN_23 = _T_23 & decode_io_alu_type == 4'h0 ? imm_gen_io_imm : _GEN_21; // @[Core.scala 130:106 Core.scala 131:20]
   reg [31:0] dt_ic_io_pc_REG; // @[Core.scala 168:31]
-  reg [63:0] dt_ic_io_instr_REG; // @[Core.scala 169:31]
   reg  dt_ic_io_wen_REG; // @[Core.scala 174:31]
   reg [63:0] dt_ic_io_wdata_REG; // @[Core.scala 175:31]
   reg [4:0] dt_ic_io_wdest_REG; // @[Core.scala 176:31]
@@ -1214,7 +1212,7 @@ module Core(
   assign dt_ic_index = 8'h0; // @[Core.scala 166:21]
   assign dt_ic_valid = 1'h1; // @[Core.scala 167:21]
   assign dt_ic_pc = {{32'd0}, dt_ic_io_pc_REG}; // @[Core.scala 168:21]
-  assign dt_ic_instr = dt_ic_io_instr_REG[31:0]; // @[Core.scala 169:21]
+  assign dt_ic_instr = io_imem_rdata[31:0]; // @[Core.scala 169:21]
   assign dt_ic_special = 8'h0; // @[Core.scala 170:21]
   assign dt_ic_skip = 1'h0; // @[Core.scala 171:21]
   assign dt_ic_isRVC = 1'h0; // @[Core.scala 172:21]
@@ -1269,7 +1267,6 @@ module Core(
       pc_en <= 1'h1; // @[Core.scala 36:7]
     end
     dt_ic_io_pc_REG <= pc; // @[Core.scala 168:31]
-    dt_ic_io_instr_REG <= io_imem_rdata; // @[Core.scala 169:31]
     dt_ic_io_wen_REG <= regfile_io_rd_en; // @[Core.scala 174:31]
     dt_ic_io_wdata_REG <= regfile_io_rd_data; // @[Core.scala 175:31]
     dt_ic_io_wdest_REG <= regfile_io_rd_addr; // @[Core.scala 176:31]
@@ -1348,18 +1345,16 @@ initial begin
   pc_en = _RAND_1[0:0];
   _RAND_2 = {1{`RANDOM}};
   dt_ic_io_pc_REG = _RAND_2[31:0];
-  _RAND_3 = {2{`RANDOM}};
-  dt_ic_io_instr_REG = _RAND_3[63:0];
-  _RAND_4 = {1{`RANDOM}};
-  dt_ic_io_wen_REG = _RAND_4[0:0];
-  _RAND_5 = {2{`RANDOM}};
-  dt_ic_io_wdata_REG = _RAND_5[63:0];
-  _RAND_6 = {1{`RANDOM}};
-  dt_ic_io_wdest_REG = _RAND_6[4:0];
+  _RAND_3 = {1{`RANDOM}};
+  dt_ic_io_wen_REG = _RAND_3[0:0];
+  _RAND_4 = {2{`RANDOM}};
+  dt_ic_io_wdata_REG = _RAND_4[63:0];
+  _RAND_5 = {1{`RANDOM}};
+  dt_ic_io_wdest_REG = _RAND_5[4:0];
+  _RAND_6 = {2{`RANDOM}};
+  cycle_cnt = _RAND_6[63:0];
   _RAND_7 = {2{`RANDOM}};
-  cycle_cnt = _RAND_7[63:0];
-  _RAND_8 = {2{`RANDOM}};
-  instr_cnt = _RAND_8[63:0];
+  instr_cnt = _RAND_7[63:0];
 `endif // RANDOMIZE_REG_INIT
   `endif // RANDOMIZE
 end // initial
