34d77856c942108bdcfc5475820fc69f818449df
tracer-oscpu2021 <tracer@oscpu.org>
2021-12-01 22:00:24 +0800

    jiangshijie 20210000    Static hostname: iscaearth          Icon name: computer-server            Chassis: server         Machine ID: 5b79f3682a3c4191a08aa2b25d1a612e            Boot ID: e382fb8501284c3492133ca9bd511cb5   Operating System: Ubuntu 20.04.3 LTS             Kernel: Linux 5.8.0-53-generic       Architecture: x86-64  22:00:24 up 186 days, 23:31, 25 users,  load average: 17.21, 18.29, 17.09

--- projects/chisel_cpu_diff/src/main/scala/Core.scala
+++ projects/chisel_cpu_diff/src/main/scala/Core.scala
@@ -27,7 +27,7 @@ class Core extends Module {
 //Initialize
 
 io.dmem.wdata := DontCare
-io.dmem.wmask := DontCare
+io.dmem.wmask := "hffffffffffffffff".U
 io.dmem.addr := DontCare
 io.dmem.wen := DontCare
 regfile.io.rd_data := DontCare
@@ -214,8 +214,27 @@ regfile.io.rd_data := Cat(Fill(32, 0.U), io.dmem.rdata(63, 32))
 
 //S-type 
 }.elsewhen(decode.io.wb_type === WB_MEM_B){
-io.dmem.wmask:= ("h00000000000000ff".U)
+
+when(io.dmem.addr(2,0)==="b000".U){
 io.dmem.wdata:= Cat(Fill(56, 0.U),regfile.io.rs2_data(7,0))
+}.elsewhen(io.dmem.addr(2,0)==="b001".U){
+io.dmem.wdata:= Cat(Fill(48, 0.U),regfile.io.rs2_data(15,8),Fill(8,0.U))
+}.elsewhen(io.dmem.addr(2,0)==="b010".U){
+io.dmem.wdata:= Cat(Fill(40, 0.U),regfile.io.rs2_data(23,16),Fill(15,0.U))
+}.elsewhen(io.dmem.addr(2,0)==="b011".U){
+io.dmem.wdata:= Cat(Fill(32, 0.U),regfile.io.rs2_data(31,24),Fill(23,0.U))
+}.elsewhen(io.dmem.addr(2,0)==="b100".U){
+io.dmem.wdata:= Cat(Fill(24, 0.U),regfile.io.rs2_data(39,32),Fill(31,0.U))
+}.elsewhen(io.dmem.addr(2,0)==="b101".U){
+io.dmem.wdata:= Cat(Fill(16, 0.U),regfile.io.rs2_data(47,40),Fill(39,0.U))
+}.elsewhen(io.dmem.addr(2,0)==="b110".U){
+io.dmem.wdata:= Cat(Fill(8, 0.U),regfile.io.rs2_data(55,48),Fill(8,0.U))
+}.elsewhen(io.dmem.addr(2,0)==="b111".U){
+io.dmem.wdata:= Cat(regfile.io.rs2_data(63,56),Fill(55,0.U))
+}
+
+
+//io.dmem.wdata:= Cat(Fill(56, 0.U),regfile.io.rs2_data(7,0))
 }.elsewhen(decode.io.wb_type === WB_MEM_H){
 io.dmem.wmask:= ("h000000000000ffff".U)
 io.dmem.wdata:= Cat(Fill(48, 0.U),regfile.io.rs2_data(15,0))
