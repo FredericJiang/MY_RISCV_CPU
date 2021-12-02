72296f4ee083e7f5ccb944e6c67b1309f84785c1
tracer-oscpu2021 <tracer@oscpu.org>
2021-12-02 16:56:45 +0800

    jiangshijie 20210000    Static hostname: iscaearth          Icon name: computer-server            Chassis: server         Machine ID: 5b79f3682a3c4191a08aa2b25d1a612e            Boot ID: e382fb8501284c3492133ca9bd511cb5   Operating System: Ubuntu 20.04.3 LTS             Kernel: Linux 5.8.0-53-generic       Architecture: x86-64  16:56:45 up 187 days, 18:28, 26 users,  load average: 4.05, 2.27, 1.44

--- projects/chisel_cpu_diff/src/main/resources/vsrc/ram_2r1w.v
+++ projects/chisel_cpu_diff/src/main/resources/vsrc/ram_2r1w.v
@@ -44,8 +44,8 @@ module ram_2r1w (
   input  [63:0] dmem_wmask,
   input         dmem_wen
 );
-  //wire [63:0] tmp;
-  //assign tmp =  {3'b000, (dmem_addr-64'h0000_0000_8000_0000) >> 3};
+  wire [63:0] tmp;
+  assign tmp =  {3'b000, (dmem_addr-64'h0000_0000_8000_0000) >> 3};
   
   wire [63:0] imem_data_0 = ram_read_helper(imem_en, {3'b000, (imem_addr - 64'h0000_0000_8000_0000) >> 3});
 
@@ -58,7 +58,7 @@ module ram_2r1w (
     ram_write_helper({3'b000, (dmem_addr-64'h0000_0000_8000_0000) >> 3}, dmem_wdata, dmem_wmask, dmem_en & dmem_wen);
     //$display("dmem_addr = %x",dmem_addr,"  dmem_data = %x",dmem_wdata);
     if(dmem_en && dmem_wen)
-    $display("dmem_addr = %x",dmem_addr,"  dmem_wdata = %x",dmem_wdata);
+    $display("dmem_addr = %x",dmem_addr,"  tmp = %x",tmp,"  dmem_wdata = %x",dmem_wdata);
 
   end
 
