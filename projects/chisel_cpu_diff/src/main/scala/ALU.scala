dab72e8302933302db7fad568f6d1103e7a86a7b
tracer-oscpu2021 <tracer@oscpu.org>
2021-12-02 21:33:14 +0800

    jiangshijie 20210000    Static hostname: iscaearth          Icon name: computer-server            Chassis: server         Machine ID: 5b79f3682a3c4191a08aa2b25d1a612e            Boot ID: e382fb8501284c3492133ca9bd511cb5   Operating System: Ubuntu 20.04.3 LTS             Kernel: Linux 5.8.0-53-generic       Architecture: x86-64  21:33:14 up 187 days, 23:04, 25 users,  load average: 18.75, 8.13, 4.91

--- projects/chisel_cpu_diff/src/main/scala/ALU.scala
+++ projects/chisel_cpu_diff/src/main/scala/ALU.scala
@@ -36,7 +36,7 @@ is(ALU_XOR)  { alu_out:= (in1 ^ in2).asUInt()}
 is(ALU_OR)   { alu_out:= (in1 | in2).asUInt()}
 is(ALU_AND)  { alu_out:= (in1 & in2).asUInt()}
 is(ALU_SLL)  { alu_out:= (in1 << shamt).asUInt()(63,0)}
-is(ALU_SLLW) { val x = (in1 << shamt).asUInt(); alu_out:= Cat(Fill(33,x(31)), x(30,0))}
+is(ALU_SLLW) { val x = (in1 << shamt(4,0)).asUInt(); alu_out:= Cat(Fill(33,x(31)), x(30,0))}
 is(ALU_SRL)  { alu_out:= (in1 >> shamt).asUInt()} //logic right shift
 is(ALU_SRLW) { val x = (in1(31,0).asUInt >> shamt).asUInt();alu_out:= Cat(Fill(32,x(31)), x(31,0))}
 is(ALU_SRA)  { alu_out:= (in1.asSInt >> shamt).asUInt()} //arithmetic right shift
