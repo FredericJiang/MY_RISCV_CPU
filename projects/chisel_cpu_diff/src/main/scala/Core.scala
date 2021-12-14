03a96bc90dac160359066d87315da0e7eff7921e
tracer-oscpu2021 <tracer@oscpu.org>
2021-12-14 11:44:55 +0800

    jiangshijie 20210000    Static hostname: iscaearth          Icon name: computer-server            Chassis: server         Machine ID: 5b79f3682a3c4191a08aa2b25d1a612e            Boot ID: e382fb8501284c3492133ca9bd511cb5   Operating System: Ubuntu 20.04.3 LTS             Kernel: Linux 5.8.0-53-generic       Architecture: x86-64  11:44:55 up 199 days, 13:16, 26 users,  load average: 2.85, 1.62, 1.38

--- projects/chisel_cpu_diff/src/main/scala/Core.scala
+++ projects/chisel_cpu_diff/src/main/scala/Core.scala
@@ -197,9 +197,9 @@ val id_op1  =  MuxCase( regfile.io.rs1_data  , Array(
                   (id_rs1_addr === 0.U && decode.io.op1_type === OP_REG) -> 0.U ,
                   (decode.io.op1_type === OP_IMM ) -> imm_gen.io.imm,
                   (decode.io.op1_type === OP_PC)  -> id_reg_pc,
-                  ((wb_reg_rd_addr  === id_rs1_addr) && (decode.io.op1_type === OP_REG) && (id_rs1_addr =/= 0.U) &&  wb_reg_rd_en) -> wb_rd_data,
                   ((mem_reg_rd_addr === id_rs1_addr) && (id_rs1_addr =/= 0.U) && mem_reg_rd_en) -> Mux(mem_reg_mem_rtype =/= MEM_X, mem_rd_data,mem_reg_alu_out),
-                  ((exe_reg_rd_addr === id_rs1_addr) && (id_rs1_addr =/= 0.U) && exe_reg_rd_en && exe_reg_mem_rtype === MEM_X) -> exe_alu_out
+                  ((exe_reg_rd_addr === id_rs1_addr) && (id_rs1_addr =/= 0.U) && exe_reg_rd_en && exe_reg_mem_rtype === MEM_X) -> exe_alu_out,
+                  ((wb_reg_rd_addr  === id_rs1_addr) && (id_rs1_addr =/= 0.U) &&  wb_reg_rd_en) -> wb_rd_data
                   ))
 
 val id_rs2 = MuxCase( regfile.io.rs2_data  , Array(
