0b79ff090b0205f74a70a50016bd0d055580749e
tracer-oscpu2021 <tracer@oscpu.org>
2021-12-17 17:19:37 +0800

    jiangshijie 20210000    Static hostname: iscaearth          Icon name: computer-server            Chassis: server         Machine ID: 5b79f3682a3c4191a08aa2b25d1a612e            Boot ID: e382fb8501284c3492133ca9bd511cb5   Operating System: Ubuntu 20.04.3 LTS             Kernel: Linux 5.8.0-53-generic       Architecture: x86-64  17:19:37 up 202 days, 18:50, 31 users,  load average: 2.77, 1.83, 1.63

--- projects/chisel_cpu_diff/src/main/scala/Core.scala
+++ projects/chisel_cpu_diff/src/main/scala/Core.scala
@@ -359,7 +359,7 @@ val dt_valid = RegInit(false.B)
 dt_valid := (wb_reg_inst =/= BUBBLE && wb_reg_inst =/= 0.U 
 && wb_reg_alu_type =/= ALU_MY_INST && !wb_reg_clint_en ) 
 
-
+when(dt_valid){
 val dt_ic = Module(new DifftestInstrCommit)
   dt_ic.io.clock    := clock
   dt_ic.io.coreid   := 0.U
@@ -403,7 +403,7 @@ when(dt_valid){
   dt_te.io.cycleCnt := cycle_cnt
   dt_te.io.instrCnt := instr_cnt
 
-
+}
 
 //printf("pc in core =%x, inst in core =%x",if_reg_pc,if_reg_inst)
 //printf("dt_ic_valid =%x, dt_pc =%x, dt_inst =%x  \n",dt_ic.io.valid ,dt_ic.io.pc ,dt_ic.io.instr)
