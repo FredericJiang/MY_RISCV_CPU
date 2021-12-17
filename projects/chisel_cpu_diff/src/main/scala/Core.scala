84cf16622dfc8ebe02e55adf49aad4aff4d0f076
tracer-oscpu2021 <tracer@oscpu.org>
2021-12-17 20:41:54 +0800

    jiangshijie 20210000    Static hostname: iscaearth          Icon name: computer-server            Chassis: server         Machine ID: 5b79f3682a3c4191a08aa2b25d1a612e            Boot ID: e382fb8501284c3492133ca9bd511cb5   Operating System: Ubuntu 20.04.3 LTS             Kernel: Linux 5.8.0-53-generic       Architecture: x86-64  20:41:54 up 202 days, 22:13, 31 users,  load average: 1.61, 1.11, 1.15

--- projects/chisel_cpu_diff/src/main/scala/Core.scala
+++ projects/chisel_cpu_diff/src/main/scala/Core.scala
@@ -412,7 +412,8 @@ when(dt_valid){
   dt_te.io.cycleCnt := cycle_cnt
   dt_te.io.instrCnt := instr_cnt
 
-val dt_csr_valid = (wb_reg_csr_type =/= CSR_X)
+val dt_csr_valid = RegInit(false.B)
+dt_csr_valid := (wb_reg_csr_type =/= CSR_X)
 
 when(dt_csr_valid){
 
